4c0f7229352b727042e6849ba33b6fe0b80ba94e
Natanae Mojica <neithanmo@gmail.com>
2015-08-12 22:57:05 -0600

    LISTO

--- gstvideo.h
+++ gstvideo.h
@@ -6,6 +6,7 @@
 #include <gst/gst.h>
 #include <gst/video/colorbalance.h>
 #include <gst/video/videooverlay.h>
+#include <QMessageBox>
 #include <QSlider>
 
 
@@ -21,24 +22,21 @@ public:
     gstvideo(QWidget *parent = 0);
     ~gstvideo();
 
-
-//protected:
-    //GMainLoop *loop;
-    //GstBus *bus;
-    //GstElement *pipeline;
-    //static void update_color_channel (const gchar *channel_name,
-                                      //gboolean increase, GstColorBalance *cb);
-
 private slots:
     void start();
     void stop();
+    void contrast(int); //slots para llamar a update_color_balance, ya que esta no se puede llamar directamente
+    void brightness(int); //debido a que SIGNALS and SLOTS deben concordar en cuanto a sus parametros
+    void hue(int);
+    void saturation(int);
+    void on_comboBox_currentIndexChanged(int index);
+
 private:
     Ui::gstvideo *ui;
     WId window;
     GstCaps *caps;
     GstElement *src;
     GstElement *conversor1;
-    GstElement *pipeline;
     GstElement *videobalance;
     GstElement *sink;
     GstBus *bus;
@@ -46,6 +44,12 @@ private:
     static GstBusSyncReply bus_sync_handler (GstBus *, GstMessage *, gpointer);
     static guintptr cam_window_handle;
     void configure();
+    void update_color_channel (gchar*, gint, GstColorBalance*);
+    static GstPadProbeReturn event_eos(GstPad *pad, GstPadProbeInfo *info, gpointer user_data);
+    static GstPadProbeReturn block_src(GstPad *pad, GstPadProbeInfo *info, gpointer user_data);
+    static gboolean to_block_src(gpointer user_data);
+    //int effect;
+    /*st-launch-1.0 v4l2src ! videoconvert ! videobalance ! videoconvert ! agingtv ! videoconvert ! autovideosink*/
 
 };
 
