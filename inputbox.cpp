b85fb67915764059a188c653d4c226806dcac737
Natanae Mojica <neithanmo@gmail.com>
2015-10-16 07:23:09 -0600

    New input Dialog for streaming settings

--- inputbox.cpp
+++ inputbox.cpp
@@ -6,9 +6,42 @@ inputBox::inputBox(QWidget *parent) :
     ui(new Ui::inputBox)
 {
     ui->setupUi(this);
+    ui->comboBox->addItems(QStringList()<<"1080p" <<"720p"
+                            <<"480p"<<"360p"<<"240p");
 }
+
 
 inputBox::~inputBox()
 {
     delete ui;
+}
+
+void inputBox::on_buttonBox_accepted()
+{
+    youtube=ui->lineEdit->text();
+}
+
+void inputBox::on_comboBox_currentIndexChanged(int index)
+{
+    int i=ui->comboBox->currentIndex();
+    switch (i){
+    case 0:
+        resolution = 1080;
+        break;
+    case 1:
+        resolution = 720;
+        break;
+    case 2:
+        resolution = 480;
+        break;
+    case 3:
+        resolution = 360;
+        break;
+    case 4:
+        resolution = 240;
+        break;
+    default:
+        resolution = 480;
+        break;
+    }
 }
