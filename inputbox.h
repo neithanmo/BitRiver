ea0724b201012d684603aae7ca3bcf3a353a48a4
Natanae Mojica <neithanmo@gmail.com>
2015-10-16 07:23:01 -0600

    New input Dialog for streaming settings

--- inputbox.h
+++ inputbox.h
@@ -2,6 +2,7 @@
 #define INPUTBOX_H
 
 #include <QDialog>
+#include <QString>
 
 namespace Ui {
 class inputBox;
@@ -14,6 +15,14 @@ class inputBox : public QDialog
 public:
     explicit inputBox(QWidget *parent = 0);
     ~inputBox();
+    QString youtube;
+    int resolution;
+
+
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_comboBox_currentIndexChanged(int index);
 
 private:
     Ui::inputBox *ui;
