afc7f1c87ca707a7577456d9660591b77194e047
Natanae Mojica <neithanmo@gmail.com>
2015-10-26 18:03:36 -0600

    BINS created - not work pipeline for now

--- gstvideo.h
+++ gstvideo.h
@@ -44,13 +44,19 @@ private:
     WId window;
     GstCaps *Vcaps; //filtro para visualizar el stream a 640x480
     GstCaps *Scaps; //resolucion del streaming, la resolucion es definida por el usuario
-    GstElement *Vsrc;
+    GstElement *Vlocalsrc;
+    GstElement *Vtcpsrc; //tcp videosource;
+    GstElement *Vfilesrc;
+    GstElement *Atcpsrc; //tcp audio source;
+    GstElement *Afilesrc; //file audio source
     GstElement *conversor1; //videoconvert for visualization
     GstElement *videobalance;
     GstElement *sink;
     GstElement *audiosink;
-    GstElement *audiosrc;
+    GstElement *Alocalsrc;
     GstElement *conv;       //audioconvert
+    GstElement *queue;
+    GstElement *audiosampler;
     // ######## Custom Bins ##########################################################################
     GstElement *abin;       //audio bin, para captura de microfono, control de volumen
                             // tambien se utiliza para el streaming
@@ -65,7 +71,7 @@ private:
     GstElement *faac;
     GstElement *h264parse;
     GstElement *tcpclientsrc;
-    GstElement *decodebin;
+    GstElement *decoder;
     GstElement *avdec_h264;
     GstElement *flvmux;
     GstElement *Ltee; //tee for live streaming and visualization on screen
@@ -81,10 +87,10 @@ private:
     //void avolume(gint);
     //int effect;
     // BIN V4L2 SOURCE/##################################
-    /*gst-launch-1.0 v4l2src ! videoconvert ! videobalance ! videoconvert ! agingtv ! videoconvert ! autovideosink*/
+    /* gst-launch-1.0 v4l2src ! videoconvert ! videobalance ! videoconvert ! agingtv ! videoconvert ! autovideosink */
 
      // BIN TCPVIDEOSRC ###########################################################################################
-      /*"tcpclientsrc host=163.178.119.218 port=5000 ! flvdemux name=demux1 ! h264parse ! avdec_h264 max-threads=4 ! videorate "*/
+      /* "tcpclientsrc host=163.178.119.218 port=5000 ! flvdemux name=demux1 ! h264parse ! avdec_h264 max-threads=4 ! videorate" */
 
 
 };
