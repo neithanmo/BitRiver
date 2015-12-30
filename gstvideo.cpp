eb1966253880f3eaed043378fa8127419474829e
Natanae Mojica <neithanmo@gmail.com>
2015-12-30 10:14:00 -0600

    cleaning no used functions

--- gstvideo.cpp
+++ gstvideo.cpp
@@ -902,8 +902,3 @@ void gstvideo::avolume(int y){
     g_print("%d \n", x);
     gst_stream_volume_set_volume (GST_STREAM_VOLUME(this->volume), GST_STREAM_VOLUME_FORMAT_LINEAR, x);
 }
-
-void gstvideo::on_lineEdit_editingFinished()
-{
-
-}
