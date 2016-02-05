24a3c371169e1833b38e0442fa3a85c6eafc075c
Natanael <neithanmo@gmail.com>
2016-02-05 16:35:59 -0600

    new dependencies list

--- moc_gstvideo.cpp
+++ moc_gstvideo.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'gstvideo.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'gstvideo.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.2.1. It"
+#error "This file was generated using the moc from 5.4.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,30 +20,31 @@
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_gstvideo_t {
     QByteArrayData data[11];
-    char stringdata[103];
+    char stringdata[102];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    offsetof(qt_meta_stringdata_gstvideo_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData) \
+    qptrdiff(offsetof(qt_meta_stringdata_gstvideo_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
     )
 static const qt_meta_stringdata_gstvideo_t qt_meta_stringdata_gstvideo = {
     {
-QT_MOC_LITERAL(0, 0, 8),
-QT_MOC_LITERAL(1, 9, 5),
-QT_MOC_LITERAL(2, 15, 0),
-QT_MOC_LITERAL(3, 16, 4),
-QT_MOC_LITERAL(4, 21, 8),
-QT_MOC_LITERAL(5, 30, 10),
-QT_MOC_LITERAL(6, 41, 3),
-QT_MOC_LITERAL(7, 45, 10),
-QT_MOC_LITERAL(8, 56, 31),
-QT_MOC_LITERAL(9, 88, 5),
-QT_MOC_LITERAL(10, 94, 7)
+QT_MOC_LITERAL(0, 0, 8), // "gstvideo"
+QT_MOC_LITERAL(1, 9, 5), // "start"
+QT_MOC_LITERAL(2, 15, 0), // ""
+QT_MOC_LITERAL(3, 16, 4), // "stop"
+QT_MOC_LITERAL(4, 21, 8), // "contrast"
+QT_MOC_LITERAL(5, 30, 10), // "brightness"
+QT_MOC_LITERAL(6, 41, 3), // "hue"
+QT_MOC_LITERAL(7, 45, 10), // "saturation"
+QT_MOC_LITERAL(8, 56, 31), // "on_comboBox_currentIndexChanged"
+QT_MOC_LITERAL(9, 88, 5), // "index"
+QT_MOC_LITERAL(10, 94, 7) // "avolume"
+
     },
     "gstvideo\0start\0\0stop\0contrast\0brightness\0"
     "hue\0saturation\0on_comboBox_currentIndexChanged\0"
-    "index\0avolume\0"
+    "index\0avolume"
 };
 #undef QT_MOC_LITERAL
 
@@ -61,14 +62,14 @@ static const uint qt_meta_data_gstvideo[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   54,    2, 0x08,
-       3,    0,   55,    2, 0x08,
-       4,    1,   56,    2, 0x08,
-       5,    1,   59,    2, 0x08,
-       6,    1,   62,    2, 0x08,
-       7,    1,   65,    2, 0x08,
-       8,    1,   68,    2, 0x08,
-      10,    1,   71,    2, 0x08,
+       1,    0,   54,    2, 0x08 /* Private */,
+       3,    0,   55,    2, 0x08 /* Private */,
+       4,    1,   56,    2, 0x08 /* Private */,
+       5,    1,   59,    2, 0x08 /* Private */,
+       6,    1,   62,    2, 0x08 /* Private */,
+       7,    1,   65,    2, 0x08 /* Private */,
+       8,    1,   68,    2, 0x08 /* Private */,
+      10,    1,   71,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -103,7 +104,7 @@ void gstvideo::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
 
 const QMetaObject gstvideo::staticMetaObject = {
     { &QWidget::staticMetaObject, qt_meta_stringdata_gstvideo.data,
-      qt_meta_data_gstvideo,  qt_static_metacall, 0, 0}
+      qt_meta_data_gstvideo,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
@@ -114,7 +115,7 @@ const QMetaObject *gstvideo::metaObject() const
 
 void *gstvideo::qt_metacast(const char *_clname)
 {
-    if (!_clname) return 0;
+    if (!_clname) return Q_NULLPTR;
     if (!strcmp(_clname, qt_meta_stringdata_gstvideo.stringdata))
         return static_cast<void*>(const_cast< gstvideo*>(this));
     return QWidget::qt_metacast(_clname);
