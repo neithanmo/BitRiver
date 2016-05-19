397fd862a79c8a7892a3009899e4bf73b1f47bf0
Natanael Mojica <neithanmo@gmail.com>
2016-05-18 19:00:19 -0600

    base source class definition. it will use for dynamics objects instances on the pipeline

--- src/datasrc.h
+++ src/datasrc.h
@@ -3,25 +3,31 @@
 #include <gst/gst.h>
 
 
+
 class Datasrc
 {
 public:
-
+    //Datasrc();
+    //~Datasrc();
+    virtual GstElement* get_bin(){return databin;}
     //virtual ~Datasrc(){ cout << "~Datasrc()";}
+    friend class gstvideo;
+
 
 protected:
+
     GstElement *databin;
     GstElement *src;
     GstElement *queue1;
     GstElement *queue2;
     GstElement *decoder;
-    virtual void datasrc_request_vpad()=0;  // video ghost src pad of databin
-    virtual void datasrc_request_apad()=0;  // audio ghost src pad of databin
+    virtual void datasrc_set_ghost_vpad()=0;  // video ghost src pad of databin
+    virtual void datasrc_set_ghost_apad()=0;  // audio ghost src pad of databin
 
 };
 #endif // DATASRC_H
 
-/*void Datasrc::datasrc_request_vpad()
+/*
 void Datasrc::datasrc_request_apad()
 {
    GstPad *pad = gst_element_get_static_pad(queue2,"src");
